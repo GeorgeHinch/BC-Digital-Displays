d017d1179467e94e94fd2dbeb5d1a32f3c382623
GeorgeHinch <George@georgehinch.com>
2016-03-22 20:40:13 -0700

    Created studio class to store machine locations

---
+++ BC Digital Displays/BC Digital Displays/Classes/Machines.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BC_Digital_Displays.Classes
+{
+    class Machines
+    {
+        [DataContract]
+        public class Studios
+        {
+            [DataMember]
+            public IList<string> Studio1 { get; set; }
+
+            [DataMember]
+            public IList<string> Studio2 { get; set; }
+
+            [DataMember]
+            public IList<string> Studio3 { get; set; }
+
+            [DataMember]
+            public IList<string> Studio4 { get; set; }
+        }
+    }
+}
